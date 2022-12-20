68e3acb1843971574b418cbc36a78d4849248e8c
stark <anishgupta0066@gmail.com>
2022-12-21 00:01:57 +0530

    successfull fetching of packages using unpkg and configured plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -8,12 +8,22 @@ export const unpkgPathPlugin = () => {
         console.log("onResolve", args);
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
             namespace: "a",
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
@@ -21,8 +31,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('tiny-test-pkg');
-              console.log(message);
+              import React, {useState} from 'react'
+              console.log(React, useState);
             `,
           };
         }
@@ -32,10 +42,12 @@ export const unpkgPathPlugin = () => {
         //     contents: 'export default "hi there!"',
         //   };
         // }
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
